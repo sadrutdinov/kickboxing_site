c36e2a026a1f489a089dd85f52089d67033751a1
Ajdar Sadrutdinov <Ajdar18@yandex.ru>
2021-11-07 21:02:21 +0400

    added information to index.html file

--- src/main/java/com/sai/kickboxing_site/controller/SiteController.java
+++ src/main/java/com/sai/kickboxing_site/controller/SiteController.java
@@ -1,4 +1,4 @@
-package com.sai.kickboxing_site;
+package com.sai.kickboxing_site.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
