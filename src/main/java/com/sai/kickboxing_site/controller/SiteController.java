c86e55a172e044c9d4a519a24a23a85b01e35480
Ajdar Sadrutdinov <Ajdar18@yandex.ru>
2021-12-06 00:15:56 +0400

    add ssl certificate

--- src/main/java/com/sai/kickboxing_site/controller/SiteController.java
+++ src/main/java/com/sai/kickboxing_site/controller/SiteController.java
@@ -1,12 +1,16 @@
 package com.sai.kickboxing_site.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @Controller
+@Slf4j
 public class SiteController {
     @GetMapping("/")
     public String showIndex() {
+        log.debug("new guest");
         return "index";
     }
 }
